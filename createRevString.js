af65bd2ca13bd723006fa3155ea164d5abc5468a
Joseph Robles Jr <rjosepha@byui.edu>
2025-04-02 10:53:16 -0600

    added a method using js provided methods.

--- createRevString.js
+++ createRevString.js
@@ -1,12 +1,12 @@
 // Assignment: Create a Function to Reverse a String
 
 // Function Declaration: Define a function named reverseString.
-    // Parameters: The function should take one parameter, a string.
-    // Function Body:
-    // Initialize an empty string to store the reversed string.
-    // Iterate through the original string from the end to the beginning.
-    // Append each character to the empty string.
-    // Return: The function should return the reversed string.
+// Parameters: The function should take one parameter, a string.
+// Function Body:
+// Initialize an empty string to store the reversed string.
+// Iterate through the original string from the end to the beginning.
+// Append each character to the empty string.
+// Return: The function should return the reversed string.
 // Example Usage
 // Call the function with a string, like "hello world".
 // Print the result to the console, which should show the reversed string.
@@ -15,7 +15,6 @@ function stringReverser(string) {
     let i = 0;
     let stringLength = string.length - 1; //to be used for the index
     let revArray = [];
-    
 
     for (item of string) {
 
@@ -23,7 +22,14 @@ function stringReverser(string) {
         i++;
     }
     let finalString = revArray.join('');
-return finalString;
+
+    return finalString;
+}
+
+function stringReverserMethods(string) {
+    return string.split('').reverse().join('');  //Uses Build-in methods
 }
 
 console.log(stringReverser("Hello World"));
+
+console.log(stringReverserMethods("Hello World"));
