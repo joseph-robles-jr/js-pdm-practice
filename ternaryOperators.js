9b83f5c27a54bdc91822e072b07897ed382fb613
Joseph Robles Jr. <156927934+joseph-robles-jr@users.noreply.github.com>
2025-04-09 16:08:56 -0600

    Worked on two more files.

--- ternaryOperators.js
+++ ternaryOperators.js
@@ -1,5 +1,6 @@
 let hello = false; //This is a boolian (True or False value). Here, we set it to equal "false".
 
+let waesaw
 function evalHello(){
 let evalHelloResults = (hello ? 'Hello, Frind!' : 'Go Away! I do not know you!');
 // Here we check the value of "hello". The "?" is the ternary operator. 
@@ -7,8 +8,33 @@ let evalHelloResults = (hello ? 'Hello, Frind!' : 'Go Away! I do not know you!')
 // The first value after the "?" is what we do if "hello" evaluates to "true".
 // The second value "Go Away! I do not know you!" will be the response if "hello" evaluates to "true".
 
+
 console.log(evalHelloResults); // Now we print the response to the console. 
 }
+
+
+function NullCheck() {
+
+let userInput; //Is a null value.  
+
+// Null coalescing operator - a backup if a value is empty 
+let userOutput = userInput ?? "Unknown User";  // Uses "Unknown Wanderer" if adventurerInput is null
+console.log("Input: " + userInput + "\nOutput: " + userOutput);
+}
+
+function checkIfExists() {
+    // Optional chaining
+    
+    let hello = {"world" : ["This is value Zero", "This is value One", "This is value Two"]}
+
+    
+
+let helloExists = hello?.world?.firstElement;  // Won't crash if dragon or vulnerabilities doesn't exist
+
+console.log(helloExists);
+}
+
+
 
 console.log(`\n`) //This prints a blank line
 
@@ -20,3 +46,7 @@ evalHello(hello=true); // we now set the value to "true". This causes the true o
 
 
 console.log(`\n`)//This prints a blank line
+
+NullCheck();
+
+checkIfExists();
--- varibaleBasics.js
+++ varibaleBasics.js
@@ -1,17 +1,46 @@
 // A variable is a way to store date. 
 // JS has several ways to store data in variables. 
 
-let yourName = "John Smith"
+// let is how we declare most variables.
 
+let yourName = "John Smith" //This is a string. 
+let isAlive = true; //This is a bool. True or False.
+let thisIsANumber = 123456789;
+
+// we can change let and var variables
+yourName = "John Doe"
+
+
+
+// We can make a constant value (It does not change).
 const hello = "Hello Mister " + yourName;
 
+// This is an Array. It can store multiple values. Arrays start at 0.
+let stuffInMyBackpacks = ["Notebook", "Calculator", "Pens", "Pencil"]; 
 
+// To read the \"Pens\" value from our array, we would run the following: 
+// Pens is the THIRD value in the array, so it is at position 2 (Positions start at 0).
+console.log(stuffInMyBackpacks[2]); 
 
-function PrintTheVariables() 
-{
-    console.log("yourName: " + yourName);
-    console.log("hello: " + hello);
-    
-
+// This is a map. It is a collection of organized data.
+let yourData = {
+    'name': yourName,
+    'Age': 24,
+    'Home Town': 'Washington D.C., USA.',
+    'Favorite Pizza Toppings': ['Cheese', 'Peperoni', 'Pineapple', 'Bacon']
 }
 
+// We can access this data with the name of it's "key" ("key" : "Value")
+console.log(yourData["Home Town"]); // This will give us the value of "Washington D.C., USA"
+
+
+
+
+
+function PrintTheVariables() {
+    // In these lines of code, we will be "Printing" the values stored in our variables. 
+    console.log("The value in yourName: " + yourName);
+    console.log("The value in hello: " + hello);
+}
+
+PrintTheVariables();
