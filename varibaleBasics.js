4f05519efad76ee17f710fcaafa94ea7284cf931
Joseph Robles Jr. <156927934+joseph-robles-jr@users.noreply.github.com>
2025-04-09 14:53:44 -0600

    added fiel.

---
+++ varibaleBasics.js
@@ -0,0 +1,17 @@
+// A variable is a way to store date. 
+// JS has several ways to store data in variables. 
+
+let yourName = "John Smith"
+
+const hello = "Hello Mister " + yourName;
+
+
+
+function PrintTheVariables() 
+{
+    console.log("yourName: " + yourName);
+    console.log("hello: " + hello);
+    
+
+}
+
